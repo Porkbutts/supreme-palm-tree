40d9a688025fc02d0c152bb4094b357789972ba0
Adrian Teng-Amnuay <pumpadrian@gmail.com>
2022-08-19 22:02:14 -0700

    Add db (#10)

---
+++ app/db.py
@@ -0,0 +1,27 @@
+"""This module wraps the mongodb interface."""
+
+import os
+from pymongo import MongoClient
+
+
+def client(username: str = None, password: str = None, base_uri: str = None) -> MongoClient:
+    """Creates the mongo client from the provided params.
+
+    Args:
+        username (str, optional): DB username. Defaults to None.
+        password (str, optional): DB password. Defaults to None.
+        base_uri (str, optional): DB base URI. Defaults to None.
+
+    Returns:
+        MongoClient: the client.
+    """
+    if not username:
+        username = os.getenv("DB_USER")
+    if not password:
+        password = os.getenv("DB_PASSWORD")
+    if not base_uri:
+        base_uri = os.getenv("DB_BASE_URI")
+
+    return MongoClient(f"mongodb+srv://{username}:{password}@{base_uri}"
+                       "/?retryWrites=true&w=majority")
+    
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 """My Module"""
 
 from typing import Union
-
+from bson import Decimal128
 from fastapi import FastAPI
+from app.db import client
+
 
 app = FastAPI()
 
@@ -18,8 +20,19 @@ async def read_item(item_id: int, query: Union[str, None] = None):
     """read_item"""
     return {"item_id": item_id, "q": query}
 
+
 @app.get("/zee")
 async def read_zee():
     """read_zee"""
     return {"What": "Up"}
-    
+
+
+@app.get("/airbnb")
+async def read_airbnb():
+    """get top 10 reviews for the airbnb collection"""
+    col = client()["sample_airbnb"]["listingsAndReviews"]
+    lst = []
+    for doc in col.find({}, {"_id": False}).limit(10):
+        encoded = {k: v.to_decimal() if isinstance(v, Decimal128) else v for k, v in doc.items()}
+        lst.append(encoded)
+    return lst
